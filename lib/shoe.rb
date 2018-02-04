65e19a67a1bcb7d2554e768ca71dbd073cc0d694
Brendan O'Dea <brendan.ode4@gmail.com>
2018-02-04 00:36:01 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,10 +1,13 @@
 class Shoe
+
+
+  attr_accessor :color, :size, :material, :condition
+  attr_reader :brand
+
   def initialize(brand)
     @brand = brand
   end
-
-  attr_accessor :color, :size, :material, :condition
-
+  
   def cobble
     self.condition = "new"
     puts "Your shoe is as good as new!"
